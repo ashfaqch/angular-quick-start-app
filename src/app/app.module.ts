3987c0a1316662416e4a5c4796835738aa8a0c4c
ash <ashfaq.choudhry@gmail.com>
2017-10-31 15:43:18 -0500

    Added module references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { NavMenuComponent } from './navmenu/navmenu.component';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
